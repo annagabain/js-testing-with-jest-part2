7945e7bdf3f88eedb0d547d6ec852ae68d1e8a97
Anna Gabain <anna.gabain@outlook.com>
2022-11-16 15:25:16 +0000

    Testing our First Function - Part 1 codealong

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ codealong mini-project with CI
 - Shows how to use tests to inform our code
 
 - By using a test-driven development approach
+
+
+Jest is a JavaScript testing framework: https://jestjs.io/
+
+It runs on Node.js (JS runtime)
+
+Jest installation steps with Node Package Manager (NPM):
+
+node -v
+npm init
+npm install --save-dev jest@26.6.3
+Testing:
+
+npm test
--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,25 @@
 // Javascript structure
 
-// game {object}
+// game {
+//     score-integer
+//     currentGame -array
+//     playerMoves -array
+//     choices -array 
+// }
 // newGame()
 // addTurn()
 // showTurns()
 // lightsOn()
 // playerTurn()
 // showScore()
+
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: []
+}
+
+
+
+module.exports = { game };
